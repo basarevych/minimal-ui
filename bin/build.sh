e0051d11c9138c795d7dc29fc7522d9d208ea127
Ross Basarevych <basarevych@gmail.com>
2018-11-16 15:34:45 +0200

    Initial commit

---
+++ bin/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+pkg=yarn
+which $pkg > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    pkg=npm
+fi
+
+rm -rf ./dist
+mkdir ./dist
+cd front
+$pkg build 
+cd ..
+cp -r front/build dist/front
+find ./dist -name '*.map' -delete
+cp -r api ./dist/api
+cp server.js dist/
+cp package.json dist/
